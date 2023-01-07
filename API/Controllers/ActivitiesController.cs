5f27e9858aeca8866cfdfb215f077553744dcfd7
Jose Luis Aguayo Aguilar <jose.aguayo@wizeline.com>
2023-01-07 00:36:37 -0600

    End of section 4

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -2,23 +2,48 @@ using Microsoft.AspNetCore.Mvc;
 using Persistence;
 using Domain;
 using Microsoft.EntityFrameworkCore;
+using MediatR;
+using Application.Activities;
 
 namespace API.Controllers //namespaces are used to organize the classes 
 {
     public class ActivitiesController : BaseApiController  //means that the class is implementing an interface
     {
-        private readonly DataContext _context; //DataContext is the source of all entities mapped over a database connection.
-        public ActivitiesController(DataContext context) //this is the constructor
-        {
-            _context = context;
-        }
+
                   // [] it is a way of specifying attributes
         [HttpGet] //api/activities  definition of the http get method
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _context.Activities.ToListAsync();
+            return await Mediator.Send(new List.Query());
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await Mediator.Send(new Details.Query{Id = id});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+            return Ok(await Mediator.Send(new Create.Command {Activity = activity}));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        {
+            activity.Id = id;
+            return Ok(await Mediator.Send(new Edit.Command{Activity = activity}));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(Guid id)
+        {
+            return Ok(await Mediator.Send(new Delete.Command{Id = id}));
+        }
 
     }
 }
+
+//CQRS  read/write database
+//concerned with the flow of the data.
